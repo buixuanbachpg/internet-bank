f1c72c7d9fa88e65a3dcbc5b403243af5324091c
bao-lg <lygiabao97@gmail.com>
2020-02-24 22:28:21 +0700

    add token

--- repos/authRepo.js
+++ repos/authRepo.js
@@ -3,6 +3,7 @@ var randtoken = require('rand-token'),
     moment = require('moment');
 
 var db = require('../fn/mysql-db'),
+    config = require('../fn/config')
     opts = require('../fn/opts');
 
 //
@@ -13,8 +14,22 @@ exports.generateAccessToken = userObj => {
         user: userObj,
         info: 'more info for you'
     }
-    var token = jwt.sign(payload, opts.ACCESS_TOKEN.SECRET_KEY, {
-        expiresIn: opts.ACCESS_TOKEN.LIFETIME
+    var token = jwt.sign(payload, config.secret, {
+        expiresIn: config.tokenLife,
+        algorithm: 'RS256'
+    });
+
+    return token;
+}
+
+exports.generateRefreshToken = userObj => {
+    var payload = {
+        user: userObj,
+        info: 'more info for you'
+    }
+    var token = jwt.sign(payload, config.refreshTokenSecret, {
+        expiresIn: config.refreshTokenLife,
+        algorithm: 'RS256'
     });
 
     return token;
@@ -23,7 +38,30 @@ exports.generateAccessToken = userObj => {
 exports.verifyAccessToken = (req, res, next) => {
     var token = req.headers['x-access-token'];
     if (token) {
-        jwt.verify(token, opts.ACCESS_TOKEN.SECRET_KEY, (err, payload) => {
+        jwt.verify(token, config.secret, { algorithm: 'RS256' }, (err, payload) => {
+            if (err) {
+                res.statusCode = 401;
+                res.json({
+                    msg: 'verify failed',
+                    error: err
+                });
+            } else {
+                req.token_payload = payload;
+                next();
+            }
+        });
+    } else {
+        res.statusCode = 403;
+        res.json({
+            msg: 'no token found'
+        });
+    }
+};
+
+exports.verifyRefreshToken = (req, res, next) => {
+    var token = req.headers['x-access-token'];
+    if (token) {
+        jwt.verify(token, config.refreshTokenSecret, { algorithm: 'RS256' }, (err, payload) => {
             if (err) {
                 res.statusCode = 401;
                 res.json({
@@ -46,32 +84,32 @@ exports.verifyAccessToken = (req, res, next) => {
 //
 // refresh-token
 
-exports.generateRefreshToken = () => {
-    return randtoken.generate(opts.REFRESH_TOKEN.SIZE);
-}
+// exports.generateRefreshToken = () => {
+//     return randtoken.generate(opts.REFRESH_TOKEN.SIZE);
+// }
 
-exports.updateRefreshToken = (id, refreshToken) => {
-    return new Promise((resolve, reject) => {
-        var rdt = moment().format('YYYY-MM-DD HH:mm:ss');
-        var sql = `delete from userRefreshTokenExt where ID = ${id}`;
-        db.delete(sql)
-            .then(affectedRows => {
-                sql = `insert into userRefreshTokenExt values(${id}, '${refreshToken}', '${rdt}')`;
-                return db.insert(sql);
-            })
-            .then(insert_id => {
-                resolve(true);
-            })
-            .catch(err => reject(err));
-    });
-}
+// exports.updateRefreshToken = (id, refreshToken) => {
+//     return new Promise((resolve, reject) => {
+//         var rdt = moment().format('YYYY-MM-DD HH:mm:ss');
+//         var sql = `delete from userRefreshTokenExt where ID = ${id}`;
+//         db.delete(sql)
+//             .then(affectedRows => {
+//                 sql = `insert into userRefreshTokenExt values(${id}, '${refreshToken}', '${rdt}')`;
+//                 return db.insert(sql);
+//             })
+//             .then(insert_id => {
+//                 resolve(true);
+//             })
+//             .catch(err => reject(err));
+//     });
+// }
 
-exports.verifyRefreshToken = refreshToken => {
-    var sql = `select * from userRefreshTokenExt where refreshToken = '${refreshToken}'`;
-    return db.load(sql);
-}
+// exports.verifyRefreshToken = refreshToken => {
+//     var sql = `select * from userRefreshTokenExt where refreshToken = '${refreshToken}'`;
+//     return db.load(sql);
+// }
 
-exports.deleteRefreshToken = id => {
-    var sql = `delete from userRefreshTokenExt where ID = ${id}`;
-    return db.delete(sql);
-}
+// exports.deleteRefreshToken = id => {
+//     var sql = `delete from userRefreshTokenExt where ID = ${id}`;
+//     return db.delete(sql);
+// }
