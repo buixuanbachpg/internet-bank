0f2b7eee5ef26a07813e6de4d34df9a18bd2fff4
bao-lg <lygiabao97@gmail.com>
2020-04-26 14:50:25 +0700

    add for danvu

--- source-internet-banking/src/app/api/user.service.ts
+++ source-internet-banking/src/app/api/user.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private base_path = 'https://bank-demo-server.herokuapp.com/users';
+  constructor(
+    private http: HttpClient
+  ) { }
+  httpOptions = (token = '') => {
+    return new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8',
+      'x-access-token': token
+    });
+  }
+
+  login<T>(user) {
+    return this.http.post<T>(`${this.base_path}/login`, JSON.stringify(user), { headers: this.httpOptions(), withCredentials: false });
+  }
+
 }
