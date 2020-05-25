4d20a3ccd24e4fd55ae58869d185a79bcd3c57ef
bao-lg <lygiabao97@gmail.com>
2020-05-25 22:33:30 +0700

    fix bug

--- source-internet-banking-admin/src/app/pages/dialog-changepass/dialog-changepass.component.ts
+++ source-internet-banking-admin/src/app/pages/dialog-changepass/dialog-changepass.component.ts
@@ -109,6 +109,7 @@ export class DialogChangepassComponent implements OnInit {
                         this.openDialog({ Text: 'Phiên làm việc đã kết thúc!', Title: 2 }).afterClosed()
                           .subscribe(
                             Prosc => {
+                              this.matDialogRef.close(false);
                               this.router.navigateByUrl('', { replaceUrl: true });
                             }
                           );
